daf1c7bdf3509f843d4841630e8438e2eb4f79a9
Filip <filam.fm@gmail.com>
2020-03-11 08:09:15 +0100

    Recommit untracked files.

---
+++ CoronaVirus/CoronaVirusTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoronaVirusTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CoronaVirusTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e716476e-cd6c-483d-bb2e-3e577ecdeeed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoronaVirus/CoronaVirusTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CoronaVirus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoronaVirusTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Coordinate coordinate;
+        [TestInitialize]
+        public void Initialize()
+        {
+            coordinate = new Coordinate((50, 2, 45.1), (15, 31, 21.3));
+        }
+        [TestMethod]
+        public void GetKilometers()
+        {
+            Assert.AreEqual(2.03, coordinate.getKilometers(new Coordinate((50, 02, 09.7), (15, 32, 47.8))));
+        }
+    }
+}
